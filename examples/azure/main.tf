98ff4e4854075117040fa77e06b8f6b618b640eb
Brendan Dalpe <brendan.dalpe@prefect.io>
2025-09-08 11:04:35 -0500

    terraform fmt

--- examples/azure/main.tf
+++ examples/azure/main.tf
@@ -5,9 +5,9 @@ provider "azurerm" {}
 module "azure_blob_to_prefect" {
   source = "prefecthq/bucket-sensor/prefect//modules/azure"
 
-  resource_group_name = "example"
+  resource_group_name  = "example"
   storage_account_name = "example"
-  location = "us-east1"
+  location             = "us-east1"
 
   # can enable if pro or enterprise, adds auth to webhook endpoint
   create_service_account = True
